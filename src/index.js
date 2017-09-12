e39ab67241a10b147dd73d4613134f5f5b5a6054
mrpegman <mrpegman@dubai>
2017-09-12 01:32:42 -0300

    metodos principais utilizando HorseManJs

--- src/index.js
+++ src/index.js
@@ -1,71 +1,93 @@
 "use strict"
 
-var Horseman = require('node-horseman');
+var Horseman = require('node-horseman')
+var co = require('co')
+
 var options = {
-	loadImages: true,
-	ignoreSSLErrors: true
+    loadImages: true,
+    ignoreSSLErrors: true
 }
+var userAgent = 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0)'
 var conta = {
-	ag: '1500',
-	cc: '14323-0'
+    ag: '1500',
+    cc: '14323-0'
 }
 
-var login = function(){
+let login = function() {
 
-	var horseman = new Horseman(options);
+    var horseman = new Horseman(options)
 
-	try{
-		let _self = horseman
-		.userAgent('Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0)')
-		.open('https://www.itau.com.br/empresas/')
-		.screenshot('antes.PNG')
-		.type('#campo_agencia', conta.ag)
-		.type('#campo_conta', conta.cc)
-		.click(".btnSubmit")
-		.waitForNextPage()
-		.waitForSelector("input[name=xsub]", { timeout : 4000});
+    try {
+        return horseman
+            .userAgent(userAgent)
+            .open('https://www.itau.com.br/empresas/')
+            .screenshot('login1.PNG')
 
-		_self
-		.screenshot('depois.PNG')
-		.then(function(address){
-			console.log(address);
-			_self.close();
-		});
-	}catch(e)
-	{
-		console.log("erro => ", e.message)
-	}
+        .type('#campo_agencia', conta.ag)
+            .type('#campo_conta', conta.cc)
+            .click(".btnSubmit")
+            .waitForNextPage()
+
+        .screenshot('login.PNG')
+            .close()
+
+    } catch (e) {
+        console.log("erro => ", e.message)
+    }
 }
 
-var loginWithEval = function(){
+let loginWithEval = function() {
 
-	var horseman = new Horseman(options);
-console.log(conta.ag)
-console.log(conta.cc)
-	try{
-		let _self = horseman
-		.userAgent('Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0)')
-		.open('https://www.itau.com.br/empresas/')
-		.screenshot('antes.PNG')
-		.evaluate(function(param) {
-			document.querySelector('#campo_agencia').value = param.ag
-			document.querySelector('#campo_conta').value = param.cc
+    var horseman = new Horseman(options)
 
-			return document.querySelector('.btnSubmit').click()
-		}, conta)
-		.waitForNextPage();
+    try {
+        return horseman
+            .userAgent(userAgent)
+            .open('https://www.itau.com.br/empresas/')
+            .screenshot('loginWithEval1.PNG')
+            .evaluate(function(param) {
+                document.querySelector('#campo_agencia').value = param.ag
+                document.querySelector('#campo_conta').value = param.cc
 
-		_self
-		.screenshot('depois.PNG')
-		.then(function(address){
-			console.log(address);
-			_self.close();
-		});
-	}catch(e)
-	{
-		console.log("erro => ", e.message)
-	}
+                return document.querySelector('.btnSubmit').click()
+            }, conta)
+            .waitForNextPage()
+
+        .screenshot('loginWithEval.PNG')
+            .close()
+
+    } catch (e) {
+        console.log("erro => ", e.message)
+    }
 }
 
+let loginYield = co.wrap(function*() {
 
-loginWithEval()
+    var horseman = new Horseman(options)
+
+    yield horseman.userAgent(userAgent)
+    yield horseman.open('https://www.itau.com.br/empresas/')
+    yield horseman.screenshot('loginYield1.PNG')
+
+    yield horseman.evaluate(function(param) {
+        document.querySelector('#campo_agencia').value = param.ag
+        document.querySelector('#campo_conta').value = param.cc
+
+        return document.querySelector('.btnSubmit').click()
+    }, conta)
+
+    yield horseman.waitForNextPage()
+
+    yield horseman.screenshot('loginYield.PNG')
+
+    return yield horseman.close();
+
+}, function(err) {
+    console.error(err.stack);
+})
+
+module.exports = {
+    login: login,
+    loginWithEval: loginWithEval,
+    loginYield: loginYield
+}
