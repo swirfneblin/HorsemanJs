7a01c7e8a0c309a4740374b13c5d60f8294e5074
Charles Damasceno <swirfneblin@gmail.com>
2017-09-12 09:56:43 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,8 +10,10 @@ var options = {
 var userAgent = 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0)'
 var conta = {
     ag: '1500',
-    cc: '14323-0'
+    cc: '2028',
+    dac: '2'
 }
+var _url = "xxxxxxxx"
 
 let login = function() {
 
@@ -20,12 +22,13 @@ let login = function() {
     try {
         return horseman
             .userAgent(userAgent)
-            .open('https://www.itau.com.br/empresas/')
+            .open(_url)
             .screenshot('login1.PNG')
 
-        .type('#campo_agencia', conta.ag)
-            .type('#campo_conta', conta.cc)
-            .click(".btnSubmit")
+        .type('#agencia', conta.ag)
+            .type('#conta', conta.cc)
+            .type('#dac', conta.dac)
+            .click("input[type=image]")
             .waitForNextPage()
 
         .screenshot('login.PNG')
@@ -43,13 +46,14 @@ let loginWithEval = function() {
     try {
         return horseman
             .userAgent(userAgent)
-            .open('https://www.itau.com.br/empresas/')
+            .open(_url)
             .screenshot('loginWithEval1.PNG')
             .evaluate(function(param) {
-                document.querySelector('#campo_agencia').value = param.ag
-                document.querySelector('#campo_conta').value = param.cc
+                document.querySelector('#agencia').value = param.ag
+                document.querySelector('#conta').value = param.cc
+                document.querySelector('#dac').value = param.dac
 
-                return document.querySelector('.btnSubmit').click()
+                return document.querySelector('input[type=image]').click()
             }, conta)
             .waitForNextPage()
 
@@ -66,14 +70,15 @@ let loginYield = co.wrap(function*() {
     var horseman = new Horseman(options)
 
     yield horseman.userAgent(userAgent)
-    yield horseman.open('https://www.itau.com.br/empresas/')
+    yield horseman.open(_url)
     yield horseman.screenshot('loginYield1.PNG')
 
     yield horseman.evaluate(function(param) {
-        document.querySelector('#campo_agencia').value = param.ag
-        document.querySelector('#campo_conta').value = param.cc
+        document.querySelector('#agencia').value = param.ag
+        document.querySelector('#conta').value = param.cc
+        document.querySelector('#dac').value = param.dac
 
-        return document.querySelector('.btnSubmit').click()
+        return document.querySelector('input[type=image]').click()
     }, conta)
 
     yield horseman.waitForNextPage()
