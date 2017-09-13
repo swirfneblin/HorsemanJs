0a0d6aefbc8487976ecb7d4f7f089ba551041825
mrpegman <mrpegman@dubai>
2017-09-12 23:56:15 -0300

    tentativa e erro

--- src/index.js
+++ src/index.js
@@ -2,18 +2,20 @@
 
 var Horseman = require('node-horseman')
 var co = require('co')
+var fs = require('fs')
 
 var options = {
     loadImages: true,
     ignoreSSLErrors: true
 }
+
 var userAgent = 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0)'
 var conta = {
-    ag: '1500',
-    cc: '2028',
+    ag: '3242',
+    cc: '13452-0',
     dac: '2'
 }
-var _url = "xxxxxxxx"
+var _url = "https://www.itau.com.br/empresas"
 
 let login = function() {
 
@@ -91,8 +93,49 @@ let loginYield = co.wrap(function*() {
     console.error(err.stack);
 })
 
+let loginYield2 = co.wrap(function*() {
+
+    ['loginYield1.PNG', 'loginYield2.PNG', 'loginYield3.PNG', 'loginYield4.PNG']
+    .forEach(function(filename) {
+        if (fs.existsSync(filename)) {
+            fs.unlink(filename);
+        }
+    })
+
+    var horseman = new Horseman(options)
+
+    yield horseman.userAgent(userAgent)
+    yield horseman.open(_url)
+    yield horseman.screenshot('loginYield1.PNG')
+
+    yield horseman.evaluate(function(param) {
+        $('#campo_agencia').val(param.ag)
+        $('#campo_conta').val(param.cc)
+        
+        $('.btnSubmit').click()
+    }, conta)
+    
+    yield horseman.waitForNextPage()
+    yield horseman.wait(5000)
+    
+    yield horseman.screenshot('loginYield2.PNG')
+
+    yield horseman.evaluate(function() {
+        $('.lnkpadrao01').click()
+    })
+    
+    yield horseman.wait(5000)
+    yield horseman.screenshot('loginYield3.PNG')
+
+    return yield horseman.close();
+
+}, function(err) {
+    console.error(err.stack);
+})
+
 module.exports = {
     login: login,
     loginWithEval: loginWithEval,
-    loginYield: loginYield
+    loginYield: loginYield,
+    loginYield2: loginYield2
 }
--- test/index.js
+++ test/index.js
@@ -7,60 +7,61 @@ var Promise = require('bluebird')
 var co = require('co')
 
 var browser = require("../index")
+browser.loginYield2()
 
-describe('Login com Horsemanjs - noParallel', function() {
-    it('Sem Eval() - preenchimento e click() como metodos nativos do HorseManJs', function() {
-        return browser.login()
-            .then(function() {
-                return Promise.fromCallback(function(done) {
-                    return fs.stat('login.PNG', done);
-                });
-            })
-            .call('isFile')
-            .should.eventually
-            .be.true()
-    })
-})
+// describe('Login com Horsemanjs - noParallel', function() {
+//     it('Sem Eval() - preenchimento e click() como metodos nativos do HorseManJs', function() {
+//         return browser.login()
+//             .then(function() {
+//                 return Promise.fromCallback(function(done) {
+//                     return fs.stat('login.PNG', done);
+//                 });
+//             })
+//             .call('isFile')
+//             .should.eventually
+//             .be.true()
+//     })
+// })
 
-parallel('Login com Horsemanjs - parallel', function() {
+// parallel('Login com Horsemanjs - parallel', function() {
 
-    it('Sem Eval() verificacao de arquivo inexistente', function() {
-        return browser.login()
-            .then(function() {
-                return Promise.fromCallback(function(done) {
-                    return fs.stat('__login.PNG', done);
-                });
-            })
-            .call('isFile')
-            .catch(function(e) {
-                return false
-            })
-            .should.eventually.be.false()
-    })
+//     it('Sem Eval() verificacao de arquivo inexistente', function() {
+//         return browser.login()
+//             .then(function() {
+//                 return Promise.fromCallback(function(done) {
+//                     return fs.stat('__login.PNG', done);
+//                 });
+//             })
+//             .call('isFile')
+//             .catch(function(e) {
+//                 return false
+//             })
+//             .should.eventually.be.false()
+//     })
 
-    it('Com Eval() - funcoes de preenchimento e click() dentro do evaluate()', function() {
-        return browser.loginWithEval()
-            .then(function() {
-                return Promise.fromCallback(function(done) {
-                    return fs.stat('loginWithEval.PNG', done);
-                });
-            })
-            .call('isFile')
-            .should.eventually
-            .be.true()
+//     it('Com Eval() - funcoes de preenchimento e click() dentro do evaluate()', function() {
+//         return browser.loginWithEval()
+//             .then(function() {
+//                 return Promise.fromCallback(function(done) {
+//                     return fs.stat('loginWithEval.PNG', done);
+//                 });
+//             })
+//             .call('isFile')
+//             .should.eventually
+//             .be.true()
 
-    })
+//     })
 
-    it('Com Generators * - utilizando async pausado', function*() {
-        return co(function*() {
-            return yield browser.loginYield()
+//     it('Com Generators * - utilizando async pausado', function*() {
+//         return co(function*() {
+//             return yield browser.loginYield()
 
-            let saved = yield Promise.fromCallback(function(done) {
-                return fs.stat('loginYield.PNG', done);
-            })
+//             let saved = yield Promise.fromCallback(function(done) {
+//                 return fs.stat('loginYield.PNG', done);
+//             })
 
-            return yield saved.call('isFile').should.eventually.be.true()
+//             return yield saved.call('isFile').should.eventually.be.true()
 
-        })
-    })
-})
+//         })
+//     })
+// })
